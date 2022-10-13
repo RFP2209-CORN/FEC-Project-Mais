be98f66f1730467f0728608546500a42aa058bdc
Josh <josh@sf-iron.com>
2022-10-13 14:09:51 -0700

    Created RelatedItemsCard skeleton and pass product info down as prop from RelatedItems map

---
+++ client/src/components/RelatedItems/RelatedItemsCard.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const RelatedItemsCard = ({item}) => {
+  const [product, setProduct] = useState(item)
+
+  return <div>An individual card</div>
+}
+
+export default RelatedItemsCard;
