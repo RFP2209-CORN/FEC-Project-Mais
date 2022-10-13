6fb11f3700bb45ab02a69068afecf8f82adf9403
Josh <josh@sf-iron.com>
2022-10-13 16:30:37 -0700

    Abstracted out calcRating, started photo assignment process

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       {/* steph */}
 
       {/* RelatedItems */}
-      {/* <RelatedItemsAndOutfits/> */}
+      <RelatedItemsAndOutfits/>
       {/* josh */}
 
       {/* QA */}
