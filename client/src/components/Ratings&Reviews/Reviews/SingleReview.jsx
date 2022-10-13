3b89217d2851b693877d3bc2061c6c2079ac0432
Scott Matsuda <smatsuda2014@gmail.com>
2022-10-13 14:35:21 -0700

    set up files for ratings and reviews

---
+++ client/src/components/Ratings&Reviews/Reviews/SingleReview.jsx
@@ -0,0 +1,13 @@
+// what a single review looks like, getting mapped from ReviewList
+
+const SingleReview = () => {
+
+  return (
+    <div>
+      Single Review
+    </div>
+  )
+
+}
+
+export default SingleReview;
