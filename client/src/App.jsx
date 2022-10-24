2334e38261c2b74b30dab5d19248e545f9e0dc18
Scott Matsuda <smatsuda2014@gmail.com>
2022-10-24 11:46:21 -0700

    took out my component in App.jsx since my last commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
       {/* Ratings */}
       {/* <RatingsBreakdownSidebar /> */}
       {/* <AddReview /> */}
-      <RatingsAndReviews />
+      {/* <RatingsAndReviews /> */}
       {/* scott */}
     </div>
   );
